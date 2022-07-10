0f8be7c15d7f0b71063a22e79692c0a31ba9bc20
meswar6 <108972388+meswar6@users.noreply.github.com>
2022-07-10 16:58:36 +0530

    Create Day4_Prgm_14.c

---
+++ Day4/Day4_Prgm_14.c
@@ -0,0 +1,14 @@
+/*Coded by Eswar Majji(69324) */
+// FINDOUT SIZE FOR ALL DATATYPES AND VARIABLES
+
+#include <stdio.h>
+
+int main()
+{
+    
+    printf("int = %ld\n", sizeof(int));
+    printf("float = %ld\n", sizeof(float));
+    printf("double = %ld\n", sizeof(double));
+    printf("char = %ld\n", sizeof(char));
+    return 0;
+}
