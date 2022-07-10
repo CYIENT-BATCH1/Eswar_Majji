e71b1df504ec1d7a0a9629b06fefc26e7555c1ce
meswar6 <108972388+meswar6@users.noreply.github.com>
2022-07-10 16:53:12 +0530

    Create Day4_Prgm9.c

---
+++ Day4/Day4_Prgm9.c
@@ -0,0 +1,13 @@
+/*Coded by Eswar Majji(69324) */
+//BIGGEST OF TWO NUMBERS USING CONDITIONAL OPERATOR
+
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2;
+    printf("Enter two numbers : ");
+    scanf("%d %d", &num1, &num2);
+    num1>num2 ? printf("Biggest among two numbers : %d",num1) : printf("Biggest among two numbers : %d",num2);
+    return 0;
+}
