20e15b4bc3ec5047b71d9b3f6aa4c3c095a64ced
meswar6 <108972388+meswar6@users.noreply.github.com>
2022-07-10 12:22:20 +0530

    Create Day1_Prgm1.c

---
+++ Day1/Day1_Prgm1.c
@@ -0,0 +1,20 @@
+/* Coded By Eswar Majji (em69324)*/
+
+//We'll say a number is special if it is a multiple of 11 or if it is one more than a multiple of 11. Print “SPECIAL” if the given non-negative number is special. Use the % "mod" operator.
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    printf("Enter the number : ");
+    scanf("%d", &num);
+    if (( ( num % 11 ) == 0) || ( ( num % 11 ) == 1))
+    {
+        printf("SPECIAL");
+    }
+    else
+    {
+        printf("NOT SPECIAL");
+    }
+    return 0;
+}
