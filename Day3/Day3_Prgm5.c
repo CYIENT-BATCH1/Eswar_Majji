5ffbb71851b0f6b6ee39547fc071f5c9e8e4dd15
meswar6 <108972388+meswar6@users.noreply.github.com>
2022-07-10 15:57:21 +0530

    Create Day3_Prgm5.c

---
+++ Day3/Day3_Prgm5.c
@@ -0,0 +1,8 @@
+//Coded by Eswar Majji(69324)
+//Right shift the value 0xFE by 3 bits and print the value
+#include <stdio.h>
+int main()
+{
+    printf("shifted num is %X", (0xFE >> 3));
+	  return 0;
+}
