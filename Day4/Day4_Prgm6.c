fdf93c3101d0497743f4d5548ff558634ce898f0
meswar6 <108972388+meswar6@users.noreply.github.com>
2022-07-10 16:42:57 +0530

    Create Day4_Prgm6.c

---
+++ Day4/Day4_Prgm6.c
@@ -0,0 +1,27 @@
+//Coded By Eswar Majji(69324)
+//RELATIONAL OPERATOR
+
+#include <stdio.h>
+int main()
+{
+	int i=10,j=2,k;
+	k = i << j;
+	printf("%d\n", k);
+
+    k = i >> j;
+    printf("%d\n", k);
+    
+    k = (i&&j) << j;
+    printf("%d\n", k);
+    
+    k = (i||j) >> j;
+    printf("%d\n", k);
+
+    k = (i&j) << i;
+    printf("%d\n", k);
+    
+    k = (i||j&&i);
+    printf("%d\n", k);
+    
+    return 0;
+}
