4ff4efddbc58d362d9f4d2433ed0a4553d5caf8d
meswar6 <108972388+meswar6@users.noreply.github.com>
2022-07-10 16:55:17 +0530

    Create Day4_Prgm_11.c

---
+++ Day4/Day4_Prgm_11.c
@@ -0,0 +1,13 @@
+/*Coded by Eswar Majji(69324) */
+//EVEN AND ODD NUMBER USING CONDITIONAL OPERATOR
+
+#include <stdio.h>
+
+int main()
+{
+    int num1;
+    printf("Enter two numbers : ");
+    scanf("%d", &num1);
+    (num1%2) == 0 ? printf("Even number") : printf("odd number");
+    return 0;
+}
