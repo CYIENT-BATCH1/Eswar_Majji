ba90a7bbd5be8fc0087d11214954c18618fbea85
meswar6 <108972388+meswar6@users.noreply.github.com>
2022-07-10 16:09:55 +0530

    Create Day3_Prgm8.c

---
+++ Day3/Day3_Prgm8.c
@@ -0,0 +1,13 @@
+////Coded by Eswar Majji(69324)
+//Write program to check given i/p by user is alphabet or not alphabet using conditional operator
+#include<stdio.h>
+
+int main()
+{
+	char a;
+	printf("Enter Character : ");
+	fflush(stdout);
+	scanf("%c", &a);
+	(a >= 'a' && a <= 'z') || (a >= 'A' && a <= 'Z')? printf("Entered input is character"): printf("Entered input is not character");
+	return 0;
+}
