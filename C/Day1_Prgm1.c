deba7dbb973c561bad3c09ebbb0018c3fb003b8f
meswar6 <108972388+meswar6@users.noreply.github.com>
2022-07-10 11:36:40 +0530

    Update Day1_Prgm1.c

--- C/Day1_Prgm1.c
+++ C/Day1_Prgm1.c
@@ -10,7 +10,13 @@ int main()
     printf("Enter the number : ");
     scanf("%d", &num);
     //is special if it is a multiple of 11 or if it is one more than a multiple of 11
-    if (( ( num % 11 ) == 0) || ( ( num % 11 ) == 1)) 
+    if (( ( num % 11 ) == 0) || ( ( num % 11 ) == 1))
+    {
         printf("SPECIAL");
+    }
+    else
+    {
+        printf("Not Special");
+    }
     return 0;
 }
